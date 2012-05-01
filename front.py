bd03592b2ced26f61576e4f45b9bac4d946f259c
Kevin Chen <321cyb@gmail.com>
2012-05-01 22:22:11 +0800

    refactor

---
+++ front.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env  python3
+#-*- coding: utf-8 -*-
+#Author: Kevin Chen
+#Date: 
+
+class Post():
+        post_id = 0
+        title = ""
+        mtime = 0 #timestamp
+        author = ""
+        markdown = ""
+        html = ""
+
+        def __init__(self, post_id, title, author, mtime, markdown = "", html = ""):
+                self.post_id = post_id
+                self.title = title
+                self.author = author
+                self.mtime = mtime
+                self.markdown = markdown
+                self.html = html
+
+
+class User():
+        name = ""
+        email = ""
+        qq = ""
+        gtalk = ""
+
+        def __init__(self, name, email = "", qq = "", gtalk = ""):
+                self.name = name
+                self.email = email
+                self.qq = qq
+                self.gtalk = gtalk
+
+
+
+
+
+# vim: ai ts=8 sts=8 et sw=8
