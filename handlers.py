f73b1ca1bcee67a5d111d3e9ccdafd9d22e83d21
Kevin Chen <321cyb@gmail.com>
2012-05-01 22:20:48 +0800

    refactor

--- handlers.py
+++ handlers.py
@@ -3,6 +3,7 @@
 import tornado.web
 
 import setting
+import front
 
 import hashlib
 import time
@@ -16,48 +17,72 @@ from  bson.objectid import ObjectId
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    user_status = "Not logged in"
-    def verifyuser(self):
-        user = self.get_secure_cookie("authenticated_user")
-        if user:
-            hit = self.application.db.users.find_one({"user": user.decode()})
-            if hit:
-                return True
-
-        return False
-
-
-class PageHandler(BaseHandler):
-    current_page = 0
-    login_success = False
-    
-    def generate_pages(self, page_list):
-        left_most = self.current_page - 4  if (self.current_page - 4) >= 1 else 1
-        right_most = self.current_page + 4 if (self.current_page + 4) <= page_list[-1] else page_list[-1]
+    user = front.User("")
+    result_message = ""
+    logged_in = False
+    def get_one_post(self, post_id):
+        '''
+        post_id ->  str type
+        return  ->  Post type
+        '''
+        post = self.application.db.posts.find_one({"_id": ObjectId(post_id)})
+        return front.Post(post_id, post["title"], post["author"], post["time"], post["markdown"], post["html"])
+    def delete_one_post(self, post_id):
+        posts = self.application.db.posts
+        posts.remove({"_id" : ObjectId(post_id)})
+  
+    def generate_pages(self, page_list, current_page):
+        left_most = current_page - 4  if (current_page - 4) >= 1 else 1
+        right_most = current_page + 4 if (current_page + 4) <= page_list[-1] else page_list[-1]
         return list(range(left_most, right_most + 1))
 
-    def get(self, pagenumber): 
-        if self.verifyuser():
-            self.user_status = self.get_secure_cookie("authenticated_user").decode()
-            self.login_success = True
-
-        self.current_page = int(pagenumber)
-        posts_number = self.application.db.posts.find().count()
+    def get_posts_of_page(self, pagenumber):
+        '''
+        pagenumber -> int type
+        return     -> ([Post], [int]), Posts  and page list
+        '''
+        posts_number = self.application.db.posts.find().count() # this number may be to be cached.
         if  posts_number > 0:
             pages, remainder = divmod(posts_number, setting.POSTS_PER_PAGE) 
             if remainder > 0:
                 pages += 1
-            page_list = self.generate_pages(list(range(1, pages + 1)))
-            if self.current_page == 1:
+            page_list = self.generate_pages(list(range(1, pages + 1)), pagenumber)
+            if pagenumber== 1:
                 posts = self.application.db.posts.find().sort("time", pymongo.DESCENDING).limit(setting.POSTS_PER_PAGE)
             else:
                 posts = self.application.db.posts.find().sort("time", pymongo.DESCENDING) \
-                            .skip((self.current_page - 1)* setting.POSTS_PER_PAGE) \
-                                    .limit(setting.POSTS_PER_PAGE)
+                        .skip((self.pagenumber - 1) * setting.POSTS_PER_PAGE) \
+                        .limit(setting.POSTS_PER_PAGE)
+
+            this_page_posts = []
+            for post in posts:
+                this_page_posts.append(front.Blog(post["_id"], post["title"], post["author"], post["time"], post["markdown"], post["html"]))
+
+            return (this_page_posts, page_list)
         else:
-            posts = []
-            page_list = []
-        self.render("home.html", posts = posts, pages= page_list, current_page = self.current_page)
+            return ([], [])
+
+
+    def get_user(self):
+        user = self.get_secure_cookie("authenticated_user")
+        if user:
+            if self.application.db.users.find_one({"user": user.decode()}):
+                return user.decode()
+
+        return None
+
+
+class PageHandler(BaseHandler):
+    login_success = False
+   
+    def get(self, pagenumber): 
+        if self.get_user():
+            self.user_status = self.get_secure_cookie("authenticated_user").decode()
+            self.login_success = True
+
+        current_page = int(pagenumber)
+        (posts, page_list) = self.get_posts_of_page(current_page)
+        self.render("home.html", posts = posts, pages= page_list, current_page = current_page)
 
 
 
@@ -101,6 +126,10 @@ class LogoutHandler(BaseHandler):
 class ComposeHandler(BaseHandler):
     compose_result = ""
     def get(self):
+        if not self.get_user():
+            self.redirect("/")
+            return
+ 
         self.render("compose.html")
 
 
@@ -108,11 +137,12 @@ class ComposeHandler(BaseHandler):
         title = self.get_argument("title")
         content = self.get_argument("content")
         html = markdown.markdown(content)
+        author = self.get_user()
 
         current_time = int(time.time())
 
         posts = self.application.db.posts
-        post = {"title": title, "content" : content, "html" : html, "time" : current_time}
+        post = {"title": title, "markdown" : content, "html" : html, "time" : current_time, "author": author}
         posts.insert(post)
         self.redirect("/")
 
@@ -120,34 +150,34 @@ class ComposeHandler(BaseHandler):
 class EditHandler(BaseHandler):
     edit_result = ""
     def get(self, edit_id):
-        if not self.verifyuser():
+        if not self.get_user():
             self.redirect("/")
             return
  
-        post = self.application.db.posts.find_one({"_id": ObjectId(edit_id)})
+        post = self.get_one_post(edit_id)
         self.render("edit.html", post = post)
 
     def post(self, edit_id):
-        title = self.get_argument("title")
-        content = self.get_argument("content")
+        title = self.get_argument("title", "")
+        content = self.get_argument("content", "")
         html = markdown.markdown(content)
 
         try:
             self.application.db.posts.update({"_id": ObjectId(edit_id)},
-                    {"$set": {"title": title, "content" : content, "html" : html}} )
+                    {"$set": {"title": title, "markdown" : content, "html" : html}} )
             self.redirect("/")
         except e:
-            self.edit_result = "update failed."
+            self.edit_result = "update failed." #Wow, this will never show up. fix later.
             self.redirect("/edit/" + edit_id)
         
 
 class DeleteHandler(BaseHandler):
     def get(self, delete_id):
-        if not self.verifyuser():
+        if not self.get_user():
             self.redirect("/")
             return
-        posts = self.application.db.posts
-        posts.remove({"_id" : ObjectId(delete_id)})
+
+        self.delete_one_post(delete_id)
         self.redirect("/")
 
 
